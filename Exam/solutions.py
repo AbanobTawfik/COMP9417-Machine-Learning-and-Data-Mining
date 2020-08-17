11cc29aa0dfad3ec13b062d123d015533171b428
Abanob Tawfik <abanob.tawfik@outlook.com>
2020-08-18 03:43:51 +1000

    cba anymore

--- Exam/solutions.py
+++ Exam/solutions.py
@@ -87,9 +87,6 @@ def quesiton_2e():
 # plotting help
 fig, ax = plt.subplots(2,1, figsize=(10,10))
 quesiton_2e()
-print(weight_plot)
-print("______")
-print(losses_2e)
 for i, ax in enumerate(ax.flat):
     if(i == 0):
         # losses is a list of 9 elements. Each element is an array of length 100 storing the loss at each iteration for that particular step size
@@ -112,19 +109,43 @@ plt.show()
 
 
 ## Question 3
-# code for part (A)
-x1s_3 = [-0.8, 3.9, 1.4, 0.1, 1.2, -2.45, -1.5, 1.2]
-x2s_3 = [1, 0.4, 1, -3.3, 2.7, 0.1, -0.5, -1.5]
-ys_3 = [1, -1, 1, -1, -1, -1, 1, 1]
-
-plt.scatter(x1s_3, x2s_3, ys_3)
-plt.show()
 
 # (c)
 # YOUR CODE HERE
+# note this is on the original feature space as i could not get (a) to work or figure out the numpy code for it to work
+# this will not converge as the space is not linearly seperable, however the general algorithm is still there, and how i print the table/ compute weights
+# is reflective of how i would of done it if i were able to figure out (a)
+def train_perceptron():
+    weights_3c = [1, 1]
+    learning_rate_3c = 0.2
+    epochs = 0
+    converged = False
+    x1s_3c = [-0.8, 3.9, 1.4, 0.1, 1.2, -2.45, -1.5, 1.2]
+    x2s_3c = [1, 0.4, 1, -3.3, 2.7, 0.1, -0.5, -1.5]
+    ys_3c = [1, -1, 1, -1, -1, -1, 1,1]
 
+    print("Iteration No. | ", " w0                          |", " w1                          | ")
+    print("---------------------------------------------------------------------------------------")
+    change_in_weight_0 = 0
+    change_in_weight_1 = 0
+    while(converged == False and epochs < 100):
+        print(epochs, " | ", weights_3c[0], " + ",change_in_weight_0, "                         | " ,weights_3c[1], " + ",change_in_weight_1)
+        change_in_weight_0 = 0
+        change_in_weight_1 = 0
+        converged = True
+        for i in range(len(x1s_3c)):
+            if ((weights_3c[0]*x1s_3c[i] + weights_3c[1]*x2s_3c[i]) * ys_3c[i]) < 0:
 
+                change_in_weight_0 += learning_rate_3c*ys_3c[i]*(x1s_3c[i]) 
+                change_in_weight_1 += learning_rate_3c*ys_3c[i]*(x2s_3c[i]) 
 
+                weights_3c[0] = weights_3c[0] + learning_rate_3c*ys_3c[i]*(x1s_3c[i])
+                weights_3c[1] = weights_3c[1] + learning_rate_3c*ys_3c[i]*(x2s_3c[i])
+                converged = False
+        epochs += 1
+
+    print("---------------------------------------------------------------------------------------")
+train_perceptron()
 
 
 # Question 5
@@ -145,6 +166,7 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_classification
+#imported to compute accuracy (was okay'd on the forum to use this)
 from sklearn.metrics import accuracy_score
 
 def create_dataset():
@@ -215,6 +237,22 @@ test_sample_size = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 models_accuracy = []
 models_timing = []
 
+def get_color_of_model(name):
+    if(name == "SVC"):
+        return "brown"
+    elif(name == "LogisticRegression"):
+        return "red"
+    elif(name == "AdaBoostClassifier"):
+        return "green"
+    elif(name == "RandomForestClassifier"):
+        return "orange"
+    elif(name == "DecisionTreeClassifier"):
+        return "blue"
+    elif(name == "MLPClassifier"):
+        return "palevioletred"
+    else:
+        return "default"
+
 # 10 iterations
 for sample_size in (test_sample_size):
     for model in (all_models):
@@ -243,8 +281,7 @@ for model in (all_models):
     for value in (filtered_model_tuple):
         x.append(value[2])
         y.append(value[1])
-    plt.plot(x, y, label = (type(model).__name__))
-
+    plt.plot(x, y, label = (type(model).__name__), color = get_color_of_model(type(model).__name__))
 
 plt.legend()
 plt.show()
@@ -260,7 +297,7 @@ for model in (all_models):
     for value in (filtered_model_tuple):
         x.append(value[2])
         y.append(value[1])
-    plt.plot(x, y, label = (type(model).__name__))
+    plt.plot(x, y, label = (type(model).__name__), color = get_color_of_model(type(model).__name__))
 
 plt.legend()
 plt.show()
